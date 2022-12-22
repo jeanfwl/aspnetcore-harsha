14c078453104844b62e29e30786df20bb750b9d7
Jean Paulo Talar <jeanttalar@gmail.com>
2022-12-22 16:55:43 -0300

    Adicionado response customizada

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+app.Run(async (HttpContext context) => {
+    context.Response.Headers["Chave"] = "Valor da Chave";
+    context.Response.Headers["Content-Type"] = "text/html";
+    await context.Response.WriteAsync("<h2>Hello World!</h2>");
+});
 
 app.Run();
